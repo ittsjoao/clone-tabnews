cd5c8db5bd7135e1329c610625dedb160b8cf728
ittsjoao <joao3mg17@gmail.com>
2025-07-30 11:56:44 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-function Home(){
-    return (
-        <div>
-            <h1>Olá Você! haha</h1>
-        </div>
-    );
-    }
-    
-    export default Home;
+function Home() {
+  return (
+    <div>
+      <h1>Olá Você! haha</h1>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentacao errada");
+}
+
+export default Home;
